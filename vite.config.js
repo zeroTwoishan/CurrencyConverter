dc5d0046cdb105bfb5eb32eec5450900eec94273
zeroTwoishan <ivankaishan09092007@gmail.com>
2026-07-10 17:13:01 +0530

    deploy: configure base path in vite config and add github actions deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CurrencyConverter/',
   plugins: [
     react(),
     tailwindcss(),
